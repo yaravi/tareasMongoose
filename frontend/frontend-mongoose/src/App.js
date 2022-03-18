5ba57dd5feb12431839d113f802b33b2eacb1722
yaravi <yaravi@gmail.com>
2022-03-17 23:25:46 -0600

    frontend 2

--- frontend/frontend-mongoose/src/App.js
+++ frontend/frontend-mongoose/src/App.js
@@ -1,9 +1,36 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import {fetchTasks} from './api'
+
+
 
 function App() {
 
+
+
   const [value, setValue] = useState("")
+  const [tasks, setTasks] = useState([])
+  console.log(tasks);
+
+  useEffect(()=> {
+    fetchTasks()
+    .then((res)=>{
+      console.log('Primer carga')
+      setTasks(res.data)
+      console.log(res.data)
+    })
+    .catch(()=> {
+      console.error(err)
+    })
+  }, [])
+
+  const addTask = () => {
+    console.log('Agrega la tarea', value)
+    setTasks(tasks.concat({
+      _id: "62314c0c96b0e5d98024cffe" + Math.floor(math.random()*10),
+      text: value
+    }))
+  }
 
   return (
     <div className="app">
@@ -14,6 +41,7 @@ function App() {
               type="text" 
               className="task-input__text"
               value={value}
+              placeholder= "Ingresa la tarea"
               onChange={(e) => {
                 setValue(e.target.value);
               }}    
@@ -27,15 +55,14 @@ function App() {
           </button>
         </div>
 
-        <div className="task">
-          <p>Esta es una tarea</p>
-        </div>
-        <div className="task">
-          <p>Esta es una tarea</p>
-        </div>
-        <div className="task">
-          <p>Esta es una tarea</p>
-        </div>
+        {tasks.map((task) => {
+          return (
+            <div className="task">
+              <p>{tasks.text}</p>
+            </div>
+          )
+        })}
+
       </header>
     </div>
   );
