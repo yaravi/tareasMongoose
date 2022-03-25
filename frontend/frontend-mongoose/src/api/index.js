9dc890bcae20b93cd3976a8f322906ef08666c2d
yaravi <yaravi@gmail.com>
2022-03-25 14:35:22 -0600

    login module

--- frontend/frontend-mongoose/src/api/index.js
+++ frontend/frontend-mongoose/src/api/index.js
@@ -3,28 +3,41 @@ import axios from 'axios';
 export const fetchTasks = (token) => {
     return axios.get('http://localhost:5001/api/tareas', {
         headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_MONGO_DB_TOKEN}`,
+        'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/x-www-form-urlencoded'
         }
     })
 }
 
-export const createTask = (text) => {
+export const createTask = (text, token) => {
     return axios.post('http://localhost:5001/api/tareas', { text }, 
     {
         headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_MONGO_DB_TOKEN}`,
+        'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/json'
         }
     })
 }
 
-export const deleteTask = (id) => {
+export const deleteTask = (id, token) => {
     return axios.delete(`http://localhost:5001/api/tareas/${id}`, 
     {
         headers: {
-        'Authorization': `Bearer ${process.env.REACT_APP_MONGO_DB_TOKEN}`,
+        'Authorization': `Bearer ${token}`,
         'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+}
+
+export const login = (email, password) => {
+    return axios.post('http://localhost:5001/api/users/login', 
+    { 
+        email, 
+        password, 
+    },
+    {
+        headers: {
+        'Content-Type': 'application/json'
         }
     })
 }
--- frontend/frontend-mongoose/src/scenes/TaskList/TaskList.js
+++ frontend/frontend-mongoose/src/scenes/TaskList/TaskList.js
@@ -5,7 +5,7 @@ import Button from '../../components/Button';
 import Task from '../../components/Task';
 import TextInput from '../../components/TextInput/TextInput';
 
-function TaskList({ user }) {
+function TaskList({ token }) { // TODO: Recibir el token como props
   const [taskText, setTaskText] = useState("")
   const [tasks, setTasks] = useState([])
   const [loader, setLoader] = useState(false)
@@ -16,7 +16,7 @@ function TaskList({ user }) {
     setLoader(true);
     const timeoutId = setTimeout(() => {
       console.log('Este proceso pasas despues de 5 segundos')
-      fetchTasks()
+      fetchTasks(token)
         .then((res) => {
           setTasks(res.data)
           setLoader(false);
@@ -32,7 +32,7 @@ function TaskList({ user }) {
   }, [])
 
   const addTask = () => {
-    createTask(taskText)
+    createTask(taskText, token)
     .then((res) => {
       const createdTask = res.data;
       setTasks(tasks.concat(createdTask))
@@ -44,7 +44,7 @@ function TaskList({ user }) {
   }
 
   const onDeleteTask = (id) => {
-    deleteTask(id)
+    deleteTask(id, token)
     .then((res) => {
       const deltedTask = res.data
       setTasks(tasks.filter((t) => t._id !== id))
@@ -56,7 +56,7 @@ function TaskList({ user }) {
 
   return (
     <div className='task-list'>
-      <h2 style={{ color: 'white' }}>Bienvenide {user}</h2>
+      <h2 style={{ color: 'white' }}>Bienvenidx</h2>
       <div className="task-input__container">
         <TextInput 
           value={taskText} 
@@ -67,7 +67,7 @@ function TaskList({ user }) {
         <Button 
           className="task-input__btn" 
           onClick={addTask}
-        >Borrar tarea</Button>
+        >Agregar tarea</Button>
       </div>
       {loader && (<p style={{ color: 'white' }}>Loading...</p>)}
       {tasks.map((task) => (
