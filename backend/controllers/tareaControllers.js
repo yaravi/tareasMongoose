10bab3fe631f200086f3278ea0e64c2174ae1941
yaravi <yaravi@gmail.com>
2022-03-29 19:15:53 -0600

    log

--- backend/controllers/tareaControllers.js
+++ backend/controllers/tareaControllers.js
@@ -4,9 +4,9 @@ const asyncHandler = require('express-async-handler')
 const Tarea = require ('../models/tareaModel')
 // CRUD FUNCTIONS
 //-------------------------------- READ
-const getTareas = asyncHandler (async(req,res) => {
-    const tareas = await Tarea.find()
-    res.status(200).json(tareas)
+const getTareas = asyncHandler(async (req, res) => {
+    const tareas = await Tarea.find({ user: req.user.id });
+    res.status(200).json(tareas);
 });
 //-------------------------------- CREATE
 const postTareas = asyncHandler (async(req,res) => {
@@ -32,11 +32,15 @@ const putTareas = asyncHandler (async(req,res) => {
         throw new Error ('Yo need to have a valid ID')
     }
     // If ID is valid then modify
+    if (tarea.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Acceso No Autorizado')
+    }
     const tareaUpdated = await Tarea.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
     })
     res.status(200).json(tareaUpdated)
-});
+})
 //-------------------------------- DELETE
 const deleteTareas = asyncHandler (async(req,res) => {
     const tarea = await Tarea.findById(req.params.id)
@@ -46,9 +50,13 @@ const deleteTareas = asyncHandler (async(req,res) => {
         throw new Error ('Yo need to have a valid ID')
     }
     // If ID is valid then modify
+    if (tarea.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Acceso No Autorizado')
+    }
     await tarea.remove()
-    res.status(200).json({id: req.params.id})
-});
+    res.status(200).json({ id: req.params.id })
+})
 // Export router route
 module.exports = {
     getTareas,
